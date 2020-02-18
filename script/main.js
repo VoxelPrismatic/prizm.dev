b99129bd7aafa0e8fbf1511e53150cc6c31e7e43
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-02-18 11:02:37 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,24 +1,3 @@
-function swapColor(color) {
-}
-
-function load(filename, aio = false) {
-    var f = new XMLHttpRequest()
-    f.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var resp = f.responseText;
-            resp = resp.trim() + "\n"
-            setHtml("file", resp)
-        }
-    }
-    f.open("GET", filename, aio);
-    f.send();
-    if(aio)
-        return new Promise(resolve => {
-            setTimeout(() => {resolve(findHtml("file"));}, 100)
-        });
-    return findHtml("file");
-}
-
 function loadPage() {
     var pages = ["about", "links"];
     var html = "";
@@ -30,5 +9,3 @@ function loadPage() {
 }
 
 window.setTimeout(loadPage, 100);
-
-
