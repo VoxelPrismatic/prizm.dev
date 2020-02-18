3b56690a9d423d1805e07932eef21f4c4fd2ed30
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-02-18 11:02:55 -0600

    Create all.js

---
+++ script/all.js
@@ -0,0 +1,20 @@
+function swapColor(color) {
+}
+
+function load(filename, aio = false) {
+    var f = new XMLHttpRequest()
+    f.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var resp = f.responseText;
+            resp = resp.trim() + "\n"
+            setHtml("file", resp)
+        }
+    }
+    f.open("GET", filename, aio);
+    f.send();
+    if(aio)
+        return new Promise(resolve => {
+            setTimeout(() => {resolve(findHtml("file"));}, 100)
+        });
+    return findHtml("file");
+}
