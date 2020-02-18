7db0f3287b28078d65e3fa00a2965eeeb6561b32
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-02-18 10:41:43 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -24,7 +24,7 @@ function loadPage() {
     var html = "";
     for(var page of pages) {
         var txt = load("/prizm.dev/text/" + page + ".txt");
-        html += `<div id="${page}" class="sect">${txt}</div>`;
+        html += `<div id="${page}" class="sect">${mark_page(txt)}</div>`;
     }
     setHtml("content", html);
 }
