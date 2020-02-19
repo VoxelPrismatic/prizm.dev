3848798aee0f7c244fa3ae917650c1acaeec54e6
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-02-19 11:05:30 -0600

    Update all.js

--- script/all.js
+++ script/all.js
@@ -1,4 +1,30 @@
-function swapColor(color) {
+var colors = {
+    "cyan": ["#0ff", "#122", "blue"],
+    "pink": ["#f08", "#221115", "pink"],
+    "blue": ["#00f", "#112", "dark"],
+    "yellow": ["#ff0", "#221", "yellow"],
+    "green": ["#0f0", "#121", "green"],
+    "orange": ["#f80", "#221511", "orange"],
+    "red": ["#f00", "#211", "red"]
+}
+
+function swapColor(colorName) {
+    var color; var bg; var name;
+    [color, bg, name] = colors[colorName];
+    for(var rule of document.styleSheets[2].cssRules) {
+        if(rule.selectorText == "h1, h2, h3, h4, h5, h6")
+            rule.style.color = color;
+        if(rule.selectorText == "a")
+            rule.style.color = color.replace(/0/gm, "a");
+        if(rule.selectorText == ".line" || rule.selectorText == ".sect") {
+            rule.style.borderTopColor = color;
+            rule.style.borderLeftColor = color;
+            rule.style.borderRightColor = color;
+            if(rule.selectorText == ".sect")
+                rule.style.borderBotttomColor = color;
+        }
+    }
+    find("logo").src = `image/priz_${name}.png`;
 }
 
 function load(filename, aio = false) {
