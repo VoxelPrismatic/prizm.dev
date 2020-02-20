b02eece320ac9dbb2cfd5ef9740e671390d8d08e
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-02-20 12:15:06 -0600

    Update com_main.js

--- script/com_main.js
+++ script/com_main.js
@@ -1,1 +1,11 @@
-
+function loadPage() {
+    var index = JSON.parse(load("/prizm.dev/data/commands.json"));
+    var st = "";
+    for(var com in index.constructor.keys(index)) {
+        var div = `<div id="${com} ${index[com]["alias"].join(" ")}">`;
+        var mark = `#] The \`${com.toUpper()}\` command\n`;
+        mark += `#Category: #${index[com]["cat"]}\n`;
+        mark += `#Aliases: \`${";]" + index[com]["alias"].join("`, `;]" || "NONE")}\`\n`;
+        
+    }
+}
