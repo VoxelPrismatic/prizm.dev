a8d7ff92737d72f300c7d07ede161724c2ff8985
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-02-19 21:49:43 -0600

    Create prizm_main.js

---
+++ script/prizm_main.js
@@ -0,0 +1,11 @@
+function loadPage() {
+    var pages = ["prizm"];
+    var html = "";
+    for(var page of pages) {
+        var txt = load("/prizm.dev/text/" + page + ".txt");
+        html += `<div id="${page}" class="sect">${mark_page(txt)}</div>`;
+    }
+    setHtml("content", html);
+}
+
+window.setTimeout(loadPage, 100);
