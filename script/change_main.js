aa6eb3e28e39e9a0e12c06ed16ab11662547d4ce
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-02-24 10:26:16 -0600

    Update change_main.js

--- script/change_main.js
+++ script/change_main.js
@@ -3,8 +3,8 @@ function loadPage() {
     var st = "";
     for(var elem of find("content").children) {
         if(elem.tagName == "h1") {
-            st += `<div id="${elem.id}~ ";
-            st += `class="collapser" onclick="find(this.id.slice(0, -1)).scrollIntoView()">
+            st += `<div id="${elem.id}~ `;
+            st += `class="collapser" onclick="scoll(this.id.slice(0, -1))"`;
             st += `${elem.id.split("_").slice(0, -1).join("_")}</div>`;
         }
     }
