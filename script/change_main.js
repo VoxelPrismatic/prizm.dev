ddf759bac84e51eb17efc0c18de0ebac9c79c012
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-02-24 10:59:42 -0600

    Update change_main.js

--- script/change_main.js
+++ script/change_main.js
@@ -2,10 +2,12 @@ function loadPage() {
     setHtml("log", mark_page(load("/prizm.dev/text/changes.txt")));
     var st = "";
     for(var elem of find("log").children) {
-        if(elem.tagName == "h1") {
-            st += `<div id="${elem.id}~ `;
-            st += `class="collapser" onclick="scoll(this.id.slice(0, -1))"`;
-            st += `${elem.id.split("_").slice(0, -1).join("_")}</div>`;
+        if(elem.tagName == "H1") {
+            var tag = "";
+            tag = `<div id="${elem.id}~" `;
+            tag += `class="lnk" onclick="scroll(this.id.slice(0, -1))">`;
+            tag += `${elem.innerHTML.split(" ", 1)[1]}</div>`;
+            st += tag;
         }
     }
     setHtml("list", st);
