3dcc82d54f2cc1f3b366219468c4a6dd74c66706
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-03-01 09:51:05 -0600

    Update md.js

--- script/md.js
+++ script/md.js
@@ -89,6 +89,7 @@ var line_regex = [
         "<div style='float: left;'>$1</div>" + 
         "<div class='dict'></div>" + 
         "<div style='float: right;'>$2</div>" + 
+        "<br style='display: none'>" +
         "<div class='dict540'></div>" + 
         "</div></br>"
     ],
