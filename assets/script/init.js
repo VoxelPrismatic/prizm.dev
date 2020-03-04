e38726a11fdbf3babc46d45ef7876adf77e0bfe4
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-03-04 17:24:13 -0600

    Update init.js

--- assets/script/init.js
+++ assets/script/init.js
@@ -9,18 +9,23 @@ var scripts = [
     "colors"
 ]
 
+
+function ready(name) {
+    console.log(name + ".js loaded");
+    if(name != "colors")
+        return;
+    startLoading();
+    window.onresize = resizeDicts;
+    window.onclick = changeFunnyTextThing;
+    window.onauxclick = changeFunnyTextThing;
+}
+
 var section = document.getElementById("scripts");
 
 for(var script of scripts) {
     var elem = document.createElement("script");
     elem.type = "text/javascript";
     elem.src = `/prizm.dev/assets/script/${script}.js`;
+    elem.onload = function(){ready(script)};
     section.appendChild(elem);
 }
-
-
-startLoading();
-
-window.onresize = resizeDicts;
-window.onclick = changeFunnyTextThing;
-window.onauxclick = changeFunnyTextThing;
