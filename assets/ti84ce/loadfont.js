2af74159d74aedb1c70827ad13972c22561ec5ff
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-03-05 22:21:03 -0600

    Update loadfont.js

--- assets/ti84ce/loadfont.js
+++ assets/ti84ce/loadfont.js
@@ -54,6 +54,10 @@ let convert = {
     "under": "_"
 }
 
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzαβγ∆δεθλµπρΣφΩστχ∠√∫°ᴛ";
+chars += "ᵀ¿¡`\ß~!@#$%^&|_…;:wuv₀₁₂₃₄₅₆₇₈₉↑↓ŜáÁàÀâÂäÄéÉèÈêÊëËíÍìÌîÎïÏóÓòÒôÔöÖúÚùÙ";
+chars += "ûÛüÜñÑçÇṗ1234567890+-*/^(){}[]'ʳχʟŚ,.␣³√ˣ≠≥≤▫⁺•·⁻ʟẋȳṗ▯▯";
+
 for(letter of "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
     letters[letter.toUpperCase()] = grabChar("big/caps/", letter);
     letters[letter.toLowerCase()] = grabChar("big/smol/", letter);
@@ -67,6 +71,15 @@ for(letter of "0123456789") {
 }
 
 for(letter of convert.constructor.keys(convert)) {
-    letters[convert[letter]] = grabChar("big/sym/", letter);
-    smols[convert[letter]] = grabChar("smol/sym/", letter);
+    letters[convert[letter]] = grabChar("big/kb/", letter);
+    smols[convert[letter]] = grabChar("smol/kb/", letter);
+}
+
+typeable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890&><@\\`|:,)(.\"=!][#-$%+^?;'/}{*~_";
+
+for(var letter of chars) {
+    if(!typeable.includes(letter)) {
+        letters[letter] = grabChar("big/uni/", letter.charCodeAt(0).toString(16).padStart(4, "0"));
+        smols[letter] = grabChar("smol/uni/", letter.charCodeAt(0).toString(16).padStart(4, "0"));
+    }
 }
