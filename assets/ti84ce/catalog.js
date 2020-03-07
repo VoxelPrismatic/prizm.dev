94a6c850f102d2f57e91e3d3ef1a3ca6e2b19dac
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-03-06 20:29:24 -0600

    Update catalog.js

--- assets/ti84ce/catalog.js
+++ assets/ti84ce/catalog.js
@@ -5,3 +5,33 @@ function randInt(mn, mx) {
 function rand() {
     return Math.random();
 }
+
+function max(...args) {
+    return Math.max(...args);
+}
+
+function min(...args) {
+    return Math.min(...args);
+}
+
+function round(num, dig = null) {
+    if(dig == null)
+        return num;
+    return Math.round(num * (10 ** dig)) / (10 ** dig);
+}
+
+function fPart(n) {
+    return n - round(n, 0);
+}
+
+function iPart(n) {
+    return round(n, 0);
+}
+
+function abs(n) {
+    return Math.abs(n);
+}
+
+function remainder(n, d) {
+    return n % d;
+}
