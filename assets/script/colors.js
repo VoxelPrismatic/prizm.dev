bc4fd288cb5ffb60ab080e5a98511054745b11bc
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-03-28 13:01:51 -0500

    Update colors.js

--- assets/script/colors.js
+++ assets/script/colors.js
@@ -30,7 +30,7 @@ function setTransitions() {
     for(var rule of rules) {
         var txt = rule.selectorText;
         if(txt) {
-            if(txt.includes("hover") || txt.includes("focus") || txt == "div")
+            if(txt.includes("hover") || txt.includes("focus") || txt == "div" || txt == "*")
                 continue;
             rule.style.transition = "all ease 1s";
         }
