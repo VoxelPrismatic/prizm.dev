5f63dc0f2c9e5542bf16bc446866a8f7587962b8
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-05 15:50:31 -0500

    Update redirect.js

--- assets/script/redirect.js
+++ assets/script/redirect.js
@@ -8,4 +8,18 @@ function didntRedirect() {
   st += "</a>, try allowing redirects... especially if you are using Firefox ;]</div>";
   document.body.innerHTML = st;
 }
+var url = "https://voxelprismatic.github.io/prizm.dev";
+var text = "home";
+if(document.URL.includes("?url=")) {
+  url = decodeUriCompontent(document.URL.split("?url=")[1]);
+  text = url;
+} else if(document.URL.includes("?page=")) {
+  var tmp = document.URL.split("?page=")[1];
+  if(urls[tmp]) {
+    url = urls[tmp];
+    text = tmp;
+  } else {
+    alert(`The redirect page '${tmp}' is invalid`);
+  }
+}
 window.location = url;
