dfba273333db5028733532553292774afd60ae61
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-05 15:37:12 -0500

    Create urls.js

---
+++ assets/script/urls.js
@@ -0,0 +1,10 @@
+var urls = {
+  "twitter": "https://twitter.com/voxelprismatic",
+  "home": "https://voxelprismatic.github.io/prizm.dev",
+  "patreon": "https://patreon.com/voxelprismatic",
+  "github": "https://github.com/voxelprismatic",
+  "discord": "https://discord.gg/Z84Nm6n",
+  "bot": "https://discordapp.com/oauth2/authorize?client_id=555862187403378699&permissions=473296118&scope=bot",
+  "reddit": "https://reddit.com/r/u_UltimateRedditCat",
+  "github": "https://github.com/VoxelPrismatic"
+};
