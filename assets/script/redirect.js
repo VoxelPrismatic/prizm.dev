ca95bef439b8032e8b3158b0dec6ccca9a7e7dda
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-05 14:48:21 -0500

    Update redirect.js

--- assets/script/redirect.js
+++ assets/script/redirect.js
@@ -1,16 +1,13 @@
-window.setTimeout(
-  function() {
-    var st = `<h1>#] REDIRECT ;]</h1><div>Hey mate, I'm redirecting you to <a href="${url}">`
-    try {
-      st += text;
-    } catch(err) {
-      st += url;
-    } 
-    st += "</a>, try allowing redirects... especially if you are using Firefox ;]</div>";
-
-    document.body.innerHTML = st;
-  }, 100
-);
+function didntRedirect() {
+  var st = `<h1>#] REDIRECT ;]</h1><div>Hey mate, I'm redirecting you to <a href="${url}">`
+  try {
+    st += text;
+  } catch(err) {
+    st += url;
+  } 
+  st += "</a>, try allowing redirects... especially if you are using Firefox ;]</div>";
+  document.body.innerHTML = st;
+}
 var head = document.getElementsByTagName("head")[0];
 var meta = document.createElement("meta");
 meta.httpEquiv = "refresh";
