83b019a738ae3d42a9e3a094944170978f8bc6dc
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-06 14:14:06 -0500

    Update shorts.js

--- assets/script/redirect/shorts.js
+++ assets/script/redirect/shorts.js
@@ -1,4 +1,4 @@
 var shorts = {
     "lUF0e8CrFIgkG1Hr": "https://github.com/voxelprismatic",
-    "x-=krfj+29ysyVyx": "https://discordapp.com/channels/474517344459161620/474524577209253890/696799119636824106"
+    "O-HJa9YcLqgmYO2Q": "https://discordapp.com/channels/474517344459161620/474524577209253890/696799119636824106"
 }
