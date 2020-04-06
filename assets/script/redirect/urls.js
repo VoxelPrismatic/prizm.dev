4d32d4f625efb9474e7f08ad7e9f7a330fddd09a
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-06 15:01:50 -0500

    Update urls.js

--- assets/script/redirect/urls.js
+++ assets/script/redirect/urls.js
@@ -4,6 +4,7 @@ var urls = {
   "patreon": "https://patreon.com/voxelprismatic",
   "github": "https://github.com/voxelprismatic",
   "discord": "https://discord.gg/Z84Nm6n",
+  "dm_dis": "https://discordapp.com/channels/@me/481591703959240706",
   "bot": "https://discordapp.com/oauth2/authorize?client_id=555862187403378699&permissions=473296118&scope=bot",
   "reddit": "https://reddit.com/r/u_UltimateRedditCat",
   "github": "https://github.com/VoxelPrismatic",
