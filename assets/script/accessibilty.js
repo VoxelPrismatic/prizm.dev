58867e199afc56c6b696a33dbdbc68e5b8f13fa7
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-12 15:19:32 -0500

    Update accessibilty.js

--- assets/script/accessibilty.js
+++ assets/script/accessibilty.js
@@ -2,26 +2,48 @@ function a11y() {
     for(var header of ["1", "2", "3", "4", "5", "6"]) {
         for(var elem of find(">h" + header)) {
             elem.tabIndex = "0";
-            elem.onkeydown = "addFocus(this)";
+            elem.onkeyup = function(evt) {
+                addFocus(evt, this);
+            }
+        }
+    }
+    
+    for(var thing of [">a", ">img"]) {
+        for(var elem of find(thing)) {
+            elem.tabIndex = "0";
+            elem.onkeyup = function(evt) {
+                addFocus(evt, this);
+            }
         }
     }
 
     for(var elem of find(">span")) {
         if(elem.className.includes("hide")) {
             elem.tabIndex = "0";
-            elem.onkeydown = "addFocus(this)";
+            elem.onkeyup = function(evt) {
+                addFocus(evt, this);
+            }
         } else {
             elem.tabIndex = "-1";
         }
     }
 }
 
-function addFocus(elem) {
+function addFocus(evt, elem) {
+    console.log(evt, elem);
+    for(var thing of find(".focusing")) {
+        thing.classList.remove("focusing");
+    }
     elem.classList.add("focusing");
-    window.setTimeout(removeFocus, 1000, elem);
-    elem.click();
+    window.setTimeout(removeFocus, 3000, elem);
+    if(evt.key == "Enter" || elem.nodeName.startsWith("H")) {
+        elem.click();
+    }
 }
 
 function removeFocus(elem) {
-    elem.classList.remove("focusing");
+    try {
+        elem.classList.remove("focusing");
+    } catch(err) {
+    }
 }
