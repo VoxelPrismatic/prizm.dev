c358313c007e55ed27a5cad30d60478061326505
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-11 19:34:36 -0500

    Create accessibilty.js

---
+++ assets/script/accessibilty.js
@@ -0,0 +1,17 @@
+function a11y() {
+    for(var header of ["1", "2", "3", "4", "5", "6"]) {
+        for(var elem of find(">h" + header)) {
+            elem.tabIndex = "0";
+            elem.onkeydown = elem.onclick;
+        }
+    }
+
+    for(var elem of find(">span")) {
+        if(span.className.includes("hide")) {
+            elem.tabIndex = "0";
+            elem.onkeydown = elem.onclick;
+        } else {
+            elem.tabIndex = "-1";
+        }
+    }
+}
