e7f4cf56827ad915d18e3b02f7948503d201afbf
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-16 10:29:31 -0500

    Create head.js

---
+++ assets/script/md/mk/head.js
@@ -0,0 +1,9 @@
+function mk_head(m, p1) {
+    for(var x = 0; x < 6; x += 1)
+        if(m[x] != "#")
+            break;
+    var id = p1.replace(/[^\w\d]/gm, "").trim();
+    var st = `<h${x} onclick="linkMe(this);"`;
+    st += `id="${id}">#] ${p1}</h${x}>`;
+    return st;
+}
