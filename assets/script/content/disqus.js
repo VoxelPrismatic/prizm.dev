99a269b306348ac9664502640a21b5f35ad5c78d
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-16 11:40:35 -0500

    Update disqus.js

--- assets/script/content/disqus.js
+++ assets/script/content/disqus.js
@@ -1,8 +1,5 @@
 function loadPage() {
     var thread = find("disqus_thread");
-    var style = thread.style;
-    thread.className = "sect";
-    style.paddingTop = "0px";
     thread.onload = delayUpdateSpacer;
     thread.onresize = delayUpdateSpacer;
 }
