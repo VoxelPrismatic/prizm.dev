570e28e00331cb0652d604c59805e9f104bafeb0
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-16 14:28:08 -0500

    Update ul.js

--- assets/script/md/mk/ul.js
+++ assets/script/md/mk/ul.js
@@ -1,11 +1,8 @@
-function mk_ul(st) {
-    var str = "";
-    var ul = [];
-    for(var line of st.split("\n").slice(0, -1))
-        ul.push(trim(line.slice(2)));
-    str += "<ul>";
-    for(var li of ul)
-        str += `<li>${mark_page(li)}</li>`;
-    str += "</ul>";
-    return "</span>"+str+"<span>";
+function mk_ol(st) {
+    var str = "<ul>";
+    st = mark_page(st);
+    for(var line of st.split("<br>")) {
+        str += "<li>" + line + "</li>";
+    }
+    return str + "</ul>"
 }
