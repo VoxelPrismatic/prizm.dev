3ecd7a7808afe99b5388581ba6380505fbefb69a
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-16 15:51:12 -0500

    Update unidata.js

--- assets/script/unidata.js
+++ assets/script/unidata.js
@@ -2,7 +2,7 @@ var uni = {};
 
 function load_unicode_index() {
     console.log("Loading unicode index... this may take a while");
-    var lines = read("/prizm.dev/assets/text/unicode_index.txt").split("\n");
+    var lines = load("/prizm.dev/assets/text/unicode_index.txt").split("\n");
     for(var line of lines) {
         if(line.trim() == "")
             continue;
