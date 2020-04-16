5288f50de5b61592d49a63924c11e3f6e5b7db14
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-16 09:20:20 -0500

    Update load.js

--- assets/script/load.js
+++ assets/script/load.js
@@ -35,7 +35,7 @@ function startLoading() {
     
         texts = load("/prizm.dev/assets/text/footer.txt", false, true).split("\n");
         changeFunnyTextThing();
-        window.onkeydown = updateSpacer;
+        window.onkeydown = delayUpdateSpacer;
     } catch(err) {
         console.info("The below error prevented the page from loading fully");
         console.error(err);
