7cb490673a5fc3590397ac84c8ee1dc13107f3df
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-16 14:27:48 -0500

    Update ol.js

--- assets/script/md/mk/ol.js
+++ assets/script/md/mk/ol.js
@@ -1,11 +1,8 @@
 function mk_ol(st) {
-    var str = "";
-    var ol = [];
-    for(var line of st.split("\n").slice(0, -1))
-        ol.push(trim(line.replace(/^\d+[\]\)\.\-] (.*)$/gm, "$1")));
-    str += "<ol>";
-    for(var li of ol)
-        str += `<li>${mark_page(li)}</li>`;
-    str += "</ol>";
-    return "</span>"+str+"<span>";
+    var str = "<ol>";
+    st = mark_page(st);
+    for(var line of st.split("<br>")) {
+        str += "<li>" + line + "</li>";
+    }
+    return str + "</ol>"
 }
