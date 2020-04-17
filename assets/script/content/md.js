96be854ad5b97996bd2abb3313cd46283094c2fb
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-17 09:19:20 -0500

    Update md.js

--- assets/script/content/md.js
+++ assets/script/content/md.js
@@ -5,9 +5,13 @@ function loadPage() {
         "class": "sect",
         "id": "markdown_interpreter",
         "<1>": {
-            "tag": "textarea",
-            "placeholder": "Type anything in here! See the output below",
-            "onkeyup": "find('markdown_output').innerHTML = mark_page(this.value)"
+            "tag": "div",
+            "style": "text-align: center",
+            "<1>": {
+                "tag": "textarea",
+                "placeholder": "Type anything in here! See the output below",
+                "onkeyup": "find('markdown_output').innerHTML = mark_page(this.value)"
+            }
         }, 
         "<2>": {
             "tag": "div",
