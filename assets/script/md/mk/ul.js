36c8dec5ebd195e1f10e17419f035bfad083d379
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-18 19:39:49 -0500

    Update ul.js

--- assets/script/md/mk/ul.js
+++ assets/script/md/mk/ul.js
@@ -2,12 +2,12 @@ function mk_ul(st, thing = "ul") {
     var str = `<${thing}>`;
     st = mark_page(st);
     for(var line of st.split("<br>")) {
-        if(line) {
-            var extra = "";
             if(line.startsWith("<ul>") || line.startsWith("<ol>")) {
-                extra = ' class="hidden-marker"';
+                str = str.slice(0, -5);
+                str += line + "</li>";
+            } else {
+                str += `<li>` + line + "</li>";
             }
-            str += `<li${extra}>` + line + "</li>";
         }
     }
     return str + `</${thing}><br>`;
