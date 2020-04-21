770e707edf8e422bca18b1a4bf0f24c42f8c92c1
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-21 10:24:18 -0500

    Update dropper.js

--- assets/script/md/mk/dropper.js
+++ assets/script/md/mk/dropper.js
@@ -1,7 +1,7 @@
 function mk_dropper(st) {
     var str = "";
     var id = st.split("\n")[0].slice(3, -3);
-    var heading = mk_head(id, id.split(" ").slice(1).join(" "));
+    var heading = mk_head(id, id.split(" ").slice(1).join(" "), false);
     id = "DROP_" + heading.split("id=")[1].split(">")[0].slice(1, -1);
     str += `<div id=${id} class="dropper dropper-closed" onclick="this.classList.toggle('dropper-closed')">`;
     str += heading + "<div>" + mark_page(st.split("\n").slice(1).join("\n"));
