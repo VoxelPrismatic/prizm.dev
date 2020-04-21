af43d0118376d5d7a4d8bd6e87cfec7f3af52630
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-21 12:10:35 -0500

    Update head.js

--- assets/script/md/mk/head.js
+++ assets/script/md/mk/head.js
@@ -1,8 +1,11 @@
 function mk_head(m, p1, escape = true, cls = "") {
+    if(cls == m) {
+        cls = "";
+    }
     for(var x = 0; x < 6; x += 1)
         if(m[x] != "#")
             break;
-    var id = p1.replace(/[^\w\d]/gm, "").trim();
+    var id_ = p1.replace(/(\\u\{.*\}|[^\w\d])/gm, "").trim();
     var st = `<h${x} onclick="linkMe(this);"`;
     var thing = "\\u{23}";
     if(!escape) {
@@ -11,6 +14,6 @@ function mk_head(m, p1, escape = true, cls = "") {
     if(cls) {
         st += ` class="${cls}"`;
     }
-    st += ` id="${id}">${thing}] ${p1}</h${x}>`;
+    st += ` id="${id_}">${thing}] ${p1}</h${x}>`;
     return st;
 }
