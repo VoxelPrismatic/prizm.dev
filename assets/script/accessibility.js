4c395e3cb65c1e7d40ee58d1e2ed0343b855f037
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-22 13:32:34 -0500

    Update accessibility.js

--- assets/script/accessibility.js
+++ assets/script/accessibility.js
@@ -1,17 +1,19 @@
 function flickery_element(h) {
     delayFunction(function(h){h.style.transition = "none";}, 1000, 1001, 1000, h);
     var shown = true;
+    var halfOpacity = compSty(h).color;
+    halfOpacity = "rgba(" + halfOpacity.split("(")[1].slice(0, -1) + ", 0.7)";
     for(var x = 1500; x <= 2500; x += Math.floor(Math.random() * 150) + 50) {
         shown = !shown;
-        if(shown) {
-            delayFunction(function(h){h.style.textShadow = "";}, x, x + 1, x, h);
+        if(!shown) {
+            delayFunction(function(h){h.style.color = halfOpacity;}, x, x + 1, x, h);
         } else {
-            delayFunction(function(h){h.style.textShadow = "none";}, x, x + 1, x, h);
+            delayFunction(function(h){h.style.color = "";}, x, x + 1, x, h);
         }
     }
     delayFunction(function(h){h.style.transition = "";}, x - 25, x, 5, h);
-    delayFunction(function(h){h.style.textShadow = "none";}, x - 50, x - 49, x, h);
-    delayFunction(function(h){h.style.textShadow = "";}, x, x + 1, x, h);
+    delayFunction(function(h){h.style.color = halfOpacity;}, x - 50, x - 49, x, h);
+    delayFunction(function(h){h.style.color = "";}, x, x + 1, x, h);
 }
 
 function flickery() {
