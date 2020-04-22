c9a3c89398f32c02220092b52d44933cd07fe8cc
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-04-22 18:34:08 -0500

    Create flicker.js

---
+++ assets/script/aesthetic/flicker.js
@@ -0,0 +1,24 @@
+function flickery_element(h) {
+    delayFunction(function(h){h.style.transition = "none";}, 1000, 1001, 1000, h);
+    var shown = true;
+    var halfOpacity = compSty(h).color;
+    halfOpacity = "rgba(" + halfOpacity.split("(")[1].slice(0, -1) + ", 0.7)";
+    for(var x = 1500; x <= 3000; x += Math.floor(Math.random() * 200) + 25) {
+        shown = !shown;
+        if(!shown) {
+            delayFunction(function(h){h.style.color = halfOpacity;}, x, x + 1, x, h);
+        } else {
+            delayFunction(function(h){h.style.color = "";}, x, x + 1, x, h);
+        }
+    }
+    delayFunction(function(h){h.style.transition = "";}, x - 25, x, 5, h);
+    delayFunction(function(h){h.style.color = halfOpacity;}, x - 50, x - 49, x, h);
+    delayFunction(function(h){h.style.color = "";}, x, x + 1, x, h);
+}
+
+function flickery() {
+    flickery_element(this);
+}
+
+flickery_element(find("head"));
+window.setInterval(flickery_element(find("head")), 15000);
