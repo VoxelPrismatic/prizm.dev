9bc9ac681f6749d53fe33918975a5f2947a9137c
VoxelPrismatic <danielpetrov@outlook.com>
2020-05-11 19:19:48 -0500

    Now redirects subfolders to subpages, eg `~/md/` will redirect to `~/md` 	modified:   assets/script/content/404.js

--- assets/script/redirect/shorts.js
+++ assets/script/redirect/shorts.js
@@ -1,5 +1,6 @@
 var shorts = {
     "lUF0e8CrFIgkG1Hr": "https://github.com/voxelprismatic",
     "O-HJa9YcLqgmYO2Q": "https://discordapp.com/channels/474517344459161620/474524577209253890/696799119636824106",
-    "B0Kf9rM_bGIWNTTo": "https://google.com/"
+    "B0Kf9rM_bGIWNTTo": "https://google.com/",
+    "2tKelOXGgkuTiiuI": "https://cdn.dlcordapp.com/attachments/486699197131915266/710452259770728448/cute_koishi.png"
 }
