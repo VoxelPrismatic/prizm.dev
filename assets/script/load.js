247d702fac1878759bf26941060ed021b0edb5af
VoxelPrismatic <danielpetrov@outlook.com>
2020-07-06 17:24:31 -0500

    another thing

--- assets/script/load.js
+++ assets/script/load.js
@@ -72,8 +72,9 @@ async function textPage(...pages) {
     for(var page of pages) {
         var txt = await load("/prizm.dev/assets/text/" + page + ".txt");
         html += `<div id="${page}" class="sect">${mark_page(txt)}</div>`;
-        find("links_and_sources").innerHTML +=
-            ` // <a href="https://github.com/VoxelPrismatic/prizm.dev/blob/master/assets/text/${page}.txt">View source [${page}]</a>`
+        if(!find("page_source_" + page))
+            find("links_and_sources").innerHTML +=
+                ` // <a id="page_source_${page}" href="https://github.com/VoxelPrismatic/prizm.dev/blob/master/assets/text/${page}.txt">View source [${page}]</a>`
     }
     setHtml("content", html);
 }
