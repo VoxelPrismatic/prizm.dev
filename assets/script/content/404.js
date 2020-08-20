dbb83dda41ac963e452ea8adcc5959ee4a7d203a
PRIZ ;] <prizmdev@outlook.com>
2020-08-20 09:36:44 -0500

    Update 404.js

--- assets/script/content/404.js
+++ assets/script/content/404.js
@@ -1,20 +1,20 @@
+async function try_url(url, not_found_page) {
+    var subpage = await load(url);
+    if(subpage != not_found_page)
+        window.location = url;
+}
+
 async function loadPage() {
     var url = document.URL.split("#")[0].split("?")[0];
     if(url.endsWith("/"))
         url = url.slice(0, -1);
-    url += document.URL.split(url)[1].slice(1);
-    var subpage = await load(url);
     var not_found_page = await load("/prizm.dev/404");
-    if(subpage != not_found_page)
-        window.location = url;
+    url += document.URL.split(url)[1].slice(1);
+    try_url(url, not_found_page);
     url += ".html";
-    var subpage = await load(url);
-    if(subpage != not_found_page)
-        window.location = url;
+    try_url(url, not_found_page);
     url = document.URL.replace("/re/", "/");
-    var subpage = await load(url);
-    if(subpage != not_found_page)
-        window.location = url;
+    await try_url(url, not_found_page);
     var data = await load("/prizm.dev/assets/data/404.json", {json: true});
     var thing = document.URL.split("/prizm.dev/")[1].split("?")[0].split("#")[0];
     var stuff = data[thing] || data[thing.slice(0, -1)];
