6e3e7b9cd94a98e09626bc03987d326fff3e96a2
PRIZ ;] <prizmdev@outlook.com>
2020-08-25 12:14:50 -0500

    Update change_main.js

--- assets/script/content/change_main.js
+++ assets/script/content/change_main.js
@@ -2,7 +2,7 @@ async function loadPage() {
     var content = await load("/prizm.dev/assets/text/changes.txt");
     setHtml("log", mark_page(content));
     var st = "";
-    for(var elem of find(">h1")) {
+    for(var elem of $all("#log h1")) {
         var tag = "";
         tag = `<div id="${elem.id}~" `;
         tag += `class="lnk" onclick="scroll_(this.id)">`;
