1252e1e41ef3ea590345f6f960650d991536172f
PRIZ ;] <prizmdev@outlook.com>
2020-08-25 12:11:00 -0500

    Update change_main.js

--- assets/script/content/change_main.js
+++ assets/script/content/change_main.js
@@ -2,14 +2,12 @@ async function loadPage() {
     var content = await load("/prizm.dev/assets/text/changes.txt");
     setHtml("log", mark_page(content));
     var st = "";
-    for(var elem of find("log").children) {
-        if(elem.tagName == "H1") {
-            var tag = "";
-            tag = `<div id="${elem.id}~" `;
-            tag += `class="lnk" onclick="scroll_(this.id)">`;
-            tag += `${elem.innerHTML.split(" ").slice(1).join(" ")}</div>`;
-            st += tag;
-        }
+    for(var elem of find(">h1")) {
+        var tag = "";
+        tag = `<div id="${elem.id}~" `;
+        tag += `class="lnk" onclick="scroll_(this.id)">`;
+        tag += `${elem.innerHTML.split(" ").slice(1).join(" ")}</div>`;
+        st += tag;
     }
     setHtml("list", st);
 }
