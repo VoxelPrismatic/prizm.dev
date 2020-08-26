6e7fb38353104faa92f44da3d7b6537a49f85f65
PRIZ ;] <prizmdev@outlook.com>
2020-08-25 19:35:58 -0500

    Update prizm_perms.txt

--- assets/script/redirect/shorts.js
+++ assets/script/redirect/shorts.js
@@ -7,5 +7,6 @@ var shorts = {
     "4l6ZYbARn8uk-O3M": "https://discordapp.com/channels/275358751630819328/275734771575816194/740244220144648192",
     "b61k": "https://discordapp.com/channels/533290351184707584/569698278271090728/740364007923253258",
     "udu2": "https://hacks.mozilla.org/2020/08/changes-to-samesite-cookie-behavior/?utm_source=dev-newsletter&utm_medium=email&utm_campaign=Aug6-2020&utm_content=samesitecookie",
-    "VvVN": "https://github.com/Marda33/SkeuoCord/releases/latest"
+    "VvVN": "https://github.com/Marda33/SkeuoCord/releases/latest",
+    "EzgI": "help"
 }
