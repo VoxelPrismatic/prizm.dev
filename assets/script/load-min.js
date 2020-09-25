9fcd4bf73d035baad8eb3bb37759a4d861a06d45
PRIZ ;] <prizmdev@outlook.com>
2020-09-25 13:10:54 -0500

    Update load-min.js

--- assets/script/load-min.js
+++ assets/script/load-min.js
@@ -15,8 +15,12 @@ async function loadNow() {
             elem.onclick = function() {
                 linkMe(this);
             }
+            elem.onfocus = function() {
+                flickery_element(this);
+            }
         }
     }
+    window.setInterval(flickery_element, 15000, $("h1#head"));
 }
 
 async function load(filename, strip = false, json = false, list = false) {
@@ -50,4 +54,21 @@ function linkMe(elem) {
         id = "#" + id;
     }
     window.history.replaceState(window.history.state, document.title, id);
+}
+
+function flickery_element(h) {
+    delayFunction(function(h){h.style.transition = "none";}, 1000, 1001, 1000, h);
+    var shown = true;
+    var halfOpacity = compSty(h).color;
+    halfOpacity = "rgba(" + halfOpacity.split("(")[1].slice(0, -1) + ", 0.7)";
+    for(var x = 1500; x <= 3000; x += Math.floor(Math.random() * 200) + 25) {
+        shown = !shown;
+        if(!shown)
+            delayFunction(function(h){h.style.color = halfOpacity;}, x, x + 1, x, h);
+        else
+            delayFunction(function(h){h.style.color = "";}, x, x + 1, x, h);
+    }
+    delayFunction(function(h){h.style.transition = "";}, x - 25, x, 5, h);
+    delayFunction(function(h){h.style.color = halfOpacity;}, x - 50, x - 49, x, h);
+    delayFunction(function(h){h.style.color = "";}, x, x + 1, x, h);
 }
