e33f90312a4bf72cb69351cd5d4ef9bf065fc7d4
VoxelPrismatic <prizmdev@outlook.com>
2021-05-26 11:14:15 -0500

    sequential loading

--- assets/script/photos.js
+++ assets/script/photos.js
@@ -1,11 +1,26 @@
 var pic_src = []
 function next_pic() {
     var src = pic_src[0];
+    if(!src)
+        return
     pic_src = pic_src.slice(1);
     $(`img[data-src="${src}"]`).src = src;
 }
-for(var img of $all("img[data-src]")) {
-    img.onload = next_pic;
-    pic_src.push(img.getAttribute("data-src"));
+function smol_pic() {
+    var src = pic_src[0];
+    if(!src) {
+        get_pic();
+        next_pic();
+        return
+    }
+    pic_src = pic_src.slice(1);
+    $(`img[data-src="${src}"]`).src = src + "-smol.webp";
 }
-next_pic();
+function get_pic() {
+    for(var img of $all("img[data-src]")) {
+        img.onload = next_pic;
+        pic_src.push(img.getAttribute("data-src"));
+    }
+}
+get_pic();
+snol_pic();
