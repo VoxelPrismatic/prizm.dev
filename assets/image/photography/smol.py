e837472aa69b804909a330ef59ffe028f79e8c4c
VoxelPrismatic <prizmdev@outlook.com>
2021-05-26 13:30:19 -0500

    smaller previews

--- assets/image/photography/smol.py
+++ assets/image/photography/smol.py
@@ -4,18 +4,27 @@ import argparse
 
 # Get target folder
 parser = argparse.ArgumentParser()
-parser.add_argument("target")
+parser.add_argument("target", nargs = "+")
 args = parser.parse_args()
-t = args.target
 
 # Start converting
-for f in os.listdir(t):
-    if f.endswith("-smol.webp"):
-        continue
-    p = ffmpeg.probe(t + "/" + f)
+ls = []
+for t in args.target:
+    for f in os.listdir(t):
+        if f.endswith("-smol.webp"):
+            continue
+        ls.append(t + "/" + f)
+m = 0
+l = len(ls)
+print()
+for f in ls:
+    m += 1
+    print(f"\x1b[1A\x1b[K{f} ({m/l*100:.2f}%)")
+    p = ffmpeg.probe(f)
     h = p["streams"][0]["height"]
     w = p["streams"][0]["width"]
-    n = 4
-    while h/n > 1024 or w/n > 1024:
+    n = 6
+    while h/n > 640 or w/n > 640:
         n += 1
-    os.system(f"ffmpeg -i \"{t}/{f}\" -vf scale={w/n:.0f}:{h/n:.0f} \"{t}/{f}-smol.webp\" -y")
+    print(f"\x1b[1A\x1b[K{f} [{m/l*100:.2f}%]")
+    os.system(f"ffmpeg -i \"{f}\" -vf scale={w/n:.0f}:{h/n:.0f} \"{f}-smol.webp\" -y 2> /dev/null")
