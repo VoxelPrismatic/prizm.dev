30cfecf2b1a494bc0fef99df4cdd7d80cfa0ed47
VoxelPrismatic <prizmdev@outlook.com>
2021-05-26 11:32:31 -0500

    fix small image loading again

--- assets/script/photos.js
+++ assets/script/photos.js
@@ -5,7 +5,9 @@ function next_pic() {
         return
     console.log(src)
     pic_src = pic_src.slice(1);
-    $(`img[data-src="${src}"]`).src = src;
+    var img = $(`img[data-src="${src}"]`)
+    img.onload = next_pic;
+    img.src = src;
 }
 function smol_pic() {
     var src = pic_src[0];
@@ -16,13 +18,13 @@ function smol_pic() {
     }
     console.log(src + "-smol.webp")
     pic_src = pic_src.slice(1);
-    $(`img[data-src="${src}"]`).src = src + "-smol.webp";
+    var img = $(`img[data-src="${src}"]`)
+    img.onload = smol_pic;
+    img.src = src + "-smol.webp";
 }
 function get_pic() {
-    for(var img of $all("img[data-src]")) {
-        img.onload = next_pic;
+    for(var img of $all("img[data-src]"))
         pic_src.push(img.getAttribute("data-src"));
-    }
 }
 get_pic();
 smol_pic();
